3c2744b83faa9c851567186c73ecd6f6365bdf35
Azra Javed <azra.javed.dev@gmail.com>
2026-08-14 10:38:18 +0500

    Implement tab navigation

--- user/app/(tabs)/_layout.tsx
+++ user/app/(tabs)/_layout.tsx
@@ -1,13 +1,45 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { useUser } from "@/hooks/useUser";
+import { Person } from "@/assets/icons/person";
+import color from "@/themes/app.colors";
+import { Car, CarPrimary, Category, Home, HomeLight } from "@/utils/icons";
+import { Tabs } from "expo-router";
+
+const getTabIcon = (routeName: string, focused: boolean) => {
+  switch (routeName) {
+    case "home":
+      return focused ? (
+        <Home colors={color.buttonBg} width={24} height={24} />
+      ) : (
+        <HomeLight />
+      );
+
+    case "services/index":
+      return <Category colors={focused ? color.buttonBg : "#8F8F8F"} />;
+
+    case "history/index":
+      return focused ? <CarPrimary /> : <Car colors="#8F8F8F" />;
+
+    case "profile/index":
+      return <Person fill={focused ? color.buttonBg : "#8F8F8F"} />;
+
+    default:
+      return null;
+  }
+};
 
 export default function _layout() {
-  const { loading, user } = useUser();
-  console.log(loading, user);
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>{user?.name}</Text>
-    </View>
+    <Tabs
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarShowLabel: false,
+
+        tabBarIcon: ({ focused }) => getTabIcon(route.name, focused),
+      })}
+    >
+      <Tabs.Screen name="home" />
+      <Tabs.Screen name="services/index" />
+      <Tabs.Screen name="history/index" />
+      <Tabs.Screen name="profile/index" />
+    </Tabs>
   );
 }
