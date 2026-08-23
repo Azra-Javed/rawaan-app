97500d6a93a52c0907b24b3fbdc5f1139a6b6f7f
Azra Javed <azra.javed.dev@gmail.com>
2026-08-23 16:30:54 +0500

    feat: add driver profile and ride history tabs

--- driver/app/(tabs)/profile/index.tsx
+++ driver/app/(tabs)/profile/index.tsx
@@ -1,12 +1,66 @@
-import { View, Text } from "react-native";
+import Button from "@/components/common/button";
+import Input from "@/components/common/input";
+import { useDriver } from "@/hooks/useDriver";
+import { fontSizes, windowHeight, windowWidth } from "@/themes/app.constant";
+import { clearAuth } from "@/utils/authStorage";
+import { router } from "expo-router";
 import React from "react";
+import { Text, View } from "react-native";
+export default function Profile() {
+  const { driver, loading } = useDriver();
 
-const Profile = () => {
+  if (loading) {
+    return;
+  }
+
+  const handlePress = async () => {
+    await clearAuth();
+    router.push("/(routes)/login");
+  };
+
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={{ paddingTop: 70 }}>
+      <Text
+        style={{
+          textAlign: "center",
+          fontSize: fontSizes.FONT30,
+          fontWeight: "600",
+        }}
+      >
+        My Profile
+      </Text>
+      <View style={{ padding: windowWidth(20) }}>
+        <Input
+          title="Name"
+          value={driver?.name}
+          onChangeText={() => console.log("")}
+          placeholder={driver?.name!}
+        />
+        <Input
+          title="Email Address"
+          value={driver?.email}
+          onChangeText={() => console.log("")}
+          placeholder={driver?.email!}
+          disabled={true}
+        />
+        <Input
+          title="Phone Number"
+          value={driver?.phone_number}
+          onChangeText={() => console.log("")}
+          placeholder={driver?.phone_number!}
+          disabled={true}
+        />
+        <View style={{ marginVertical: 25 }}>
+          <Button
+            onPress={async () => {
+              handlePress();
+            }}
+            title="Log Out"
+            height={windowHeight(35)}
+            backgroundColor="crimson"
+          />
+        </View>
+      </View>
     </View>
   );
-};
-
-export default Profile;
+}
--- driver/app/(tabs)/rides/index.tsx
+++ driver/app/(tabs)/rides/index.tsx
@@ -1,12 +1,45 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
 
-const Rides = () => {
+import RideCard from "@/components/ride/ride.card";
+import color from "@/themes/app.colors";
+
+import api from "@/api/client";
+import { windowHeight } from "@/themes/app.constant";
+import styles from "@/secreens/home/styles";
+
+export default function Rides() {
+  const [recentRides, setrecentRides] = useState([]);
+
+  useEffect(() => {
+    const getRecentRides = async () => {
+      const res = await api.get(`/driver/get-rides`);
+      setrecentRides(res.data.rides);
+    };
+
+    getRecentRides();
+  }, []);
+
   return (
-    <View>
-      <Text>Rides</Text>
+    <View
+      style={[
+        styles.rideContainer,
+        { backgroundColor: color.lightGray, paddingTop: windowHeight(40) },
+      ]}
+    >
+      <Text
+        style={[
+          styles.rideTitle,
+          { color: color.primaryText, fontWeight: "600" },
+        ]}
+      >
+        Ride History
+      </Text>
+      <ScrollView>
+        {recentRides?.map((item: any, index: number) => (
+          <RideCard item={item} key={index} />
+        ))}
+      </ScrollView>
     </View>
   );
-};
-
-export default Rides;
+}
