e11623ddc90173c8030397d3c2f1c31f38d6e3f0
Azra Javed <azra.javed.dev@gmail.com>
2026-08-23 16:20:25 +0500

    feat: enhance profile screen with user input fields and logout functionality

--- user/app/(tabs)/history/index.tsx
+++ user/app/(tabs)/history/index.tsx
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { View, Text, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import styles from "@/screens/home/styles";
+import color from "@/themes/app.colors";
+import RideCard from "@/components/ride/ride.card";
+import { windowHeight } from "@/themes/app.constant";
+import api from "@/api/client";
 
-const History = () => {
+export default function History() {
+  const [recentRides, setrecentRides] = useState([]);
+
+  useEffect(() => {
+    const getRecentRides = async () => {
+      const res = await api.get(`/user/get-rides`);
+      setrecentRides(res.data.rides);
+    };
+    getRecentRides();
+  }, []);
+
   return (
-    <View>
-      <Text>History</Text>
+    <View
+      style={[
+        styles.rideContainer,
+        { backgroundColor: color.lightGray, paddingTop: windowHeight(40) },
+      ]}
+    >
+      <Text
+        style={[
+          styles.rideTitle,
+          { color: color.primaryText, fontWeight: "600" },
+        ]}
+      >
+        Ride History
+      </Text>
+      <ScrollView>
+        {recentRides?.map((item: any, index: number) => (
+          <RideCard item={item} key={index} />
+        ))}
+      </ScrollView>
     </View>
   );
-};
-
-export default History;
-
-const styles = StyleSheet.create({});
+}
--- user/app/(tabs)/profile/index.tsx
+++ user/app/(tabs)/profile/index.tsx
@@ -1,14 +1,66 @@
-import { StyleSheet, Text, View } from "react-native";
+import Input from "@/components/common/input";
+import { fontSizes, windowWidth } from "@/themes/app.constant";
 import React from "react";
+import { Text, View } from "react-native";
 
-const Profile = () => {
+import Button from "@/components/common/button";
+
+import { useUser } from "@/hooks/useUser";
+import { clearAuth } from "@/utils/authStorage";
+import { router } from "expo-router";
+
+export default function Profile() {
+  const { user, loading } = useUser();
+
+  if (loading) {
+    return;
+  }
+
+  const handlePress = async () => {
+    await clearAuth();
+    router.push("/(routes)/login");
+  };
+
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={{ paddingTop: 70 }}>
+      <Text
+        style={{
+          textAlign: "center",
+          fontSize: fontSizes.FONT30,
+          fontWeight: "600",
+        }}
+      >
+        My Profile
+      </Text>
+      <View style={{ padding: windowWidth(20) }}>
+        <Input
+          title="Name"
+          value={user?.name}
+          onChangeText={() => console.log("")}
+          placeholder={user?.name!}
+        />
+        <Input
+          title="Email Address"
+          value={user?.email}
+          onChangeText={() => console.log("")}
+          placeholder={user?.email!}
+          disabled={true}
+        />
+        <Input
+          title="Phone Number"
+          value={user?.phone_number}
+          onChangeText={() => console.log("")}
+          placeholder={user?.phone_number!}
+          disabled={true}
+        />
+        <View style={{ marginVertical: 25 }}>
+          <Button
+            onPress={() => handlePress()}
+            title="Log Out"
+            backgroundColor="crimson"
+          />
+        </View>
+      </View>
     </View>
   );
-};
-
-export default Profile;
-
-const styles = StyleSheet.create({});
+}
